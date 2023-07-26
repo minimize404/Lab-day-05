ed0743ea1bff78f98748b5232cb0d103f96e7f2e
Minindu Gunawardana <127023179+minimize404@users.noreply.github.com>
2023-07-26 15:09:06 +0530

    Add files via upload

---
+++ Lab day 5/Q1/ConvertValues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23052023___Lab_Session
+{
+    public class ConvertValues
+    {
+        public void kilometerToMeterNoReturnNoParameter()
+        {
+            Console.Write("Enter the kilometer value: ");
+            double kmValue = double.Parse(Console.ReadLine());
+            Console.WriteLine(kmValue + " km = " + (kmValue * 1000) + " m");
+
+        }
+
+        public void kilometerToMeterNoReturn(double kmValue)
+        {
+            Console.WriteLine(kmValue + " km = " + (kmValue * 1000) + " m");
+        }
+
+        public double kilometerToMeter(double kmValue)
+        {
+            return kmValue * 1000;
+        }
+    }
+}
---
+++ Lab day 5/Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23052023___Lab_Session
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues convertValuesObj = new ConvertValues();
+            convertValuesObj.kilometerToMeterNoReturnNoParameter();
+
+            Console.Write("Enter the kilometer value: ");
+            double kmValue = double.Parse(Console.ReadLine());
+            convertValuesObj.kilometerToMeterNoReturn(kmValue);
+
+            // Test
+        }
+    }
+}
